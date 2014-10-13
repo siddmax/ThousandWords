8ed85242777a16cef07984c05a8dafae6ae396bf
Saurav Sharma <Neil@sauravsrmasmbp2.att.net>
2014-10-13 11:59:10 -0700

    Initial Commit

---
+++ ThousandWords/SSAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  SSAppDelegate.h
+//  ThousandWords
+//
+//  Created by Saurav Sharma on 10/13/14.
+//  Copyright (c) 2014 ZineOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ ThousandWords/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ThousandWords
+//
+//  Created by Saurav Sharma on 10/13/14.
+//  Copyright (c) 2014 ZineOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SSAppDelegate class]));
+    }
+}
